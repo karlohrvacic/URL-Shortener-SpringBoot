68cde308300a9c4258ac4435a711a9703171ac43
Karlo Hrvačić <karlo.hrvacic@biss.hr>
2022-05-09 19:02:02 +0200

    Root redirects to frontend

--- src/main/java/codes/karlo/api/controller/UrlController.java
+++ src/main/java/codes/karlo/api/controller/UrlController.java
@@ -11,39 +11,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
 
 @CommonsLog
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("api/v1/url")
 @RequiredArgsConstructor
 public class UrlController {
 
     private final UrlService urlService;
 
-    @PostMapping("/api/v1/url/new")
+    @PostMapping("/new")
     public Url saveUrlRandomShortUrl(@Valid @RequestBody final Url url) {
         return urlService.saveUrlRandomShortUrl(url);
     }
 
-    @PostMapping("/api/v1/url/new/{apiKey}")
+    @PostMapping("/new/{apiKey}")
     public Url saveUrlWithApiKey(@Valid @RequestBody final Url url,
                                  @PathVariable(required = false, value = "apiKey") final String apiKey) {
         return urlService.saveUrlWithApiKey(url, apiKey);
+
     }
 
-    @GetMapping({"/api/v1/url/redirect/{short}"})
+    @GetMapping("/redirect/{short}")
     public Url fetchUrlByShort(@PathVariable("short") final String shortUrl) {
         return urlService.fetchUrlByShortUrl(shortUrl);
     }
 
-    @GetMapping({"/{short}"})
-    public RedirectView fetchUrlByShortUrlOrRedirectToFrontend(@PathVariable("short") final String shortUrl) {
-        return urlService.redirectResultUrl(shortUrl);
-    }
-
-    @GetMapping("/api/v1/url/my-urls/{apiKey}")
+    @GetMapping("/my-urls/{apiKey}")
     public List<Url> fetchUrls(@PathVariable("apiKey") final String apiKey) {
         return urlService.fetchUrls(apiKey);
     }
---
+++ src/main/java/codes/karlo/api/controller/UrlRedirectController.java
@@ -0,0 +1,29 @@
+package codes.karlo.api.controller;
+
+import codes.karlo.api.service.UrlService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.apachecommons.CommonsLog;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@CommonsLog
+@RestController
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+public class UrlRedirectController {
+
+    private final UrlService urlService;
+
+    @GetMapping({"/{short}"})
+    public RedirectView fetchUrlByShortUrlOrRedirectToFrontend(@PathVariable("short") final String shortUrl) {
+        return urlService.redirectResultUrl(shortUrl);
+    }
+
+    @GetMapping({"/"})
+    public RedirectView rootRedirectToFrontend() {
+        return urlService.redirectResultUrl(null);
+    }
+}
