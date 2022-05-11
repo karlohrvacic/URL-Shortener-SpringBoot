89822c2c3adeb29a39d606b341e3f6d475936713
Karlo Hrvačić <karlo.hrvacic@biss.hr>
2022-05-11 19:11:01 +0200

    Annotations on properties

--- src/main/java/codes/karlo/api/config/AppProperties.java
+++ src/main/java/codes/karlo/api/config/AppProperties.java
@@ -1,5 +1,8 @@
 package codes.karlo.api.config;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
@@ -8,11 +11,30 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @ConfigurationProperties("app")
 public class AppProperties {
+
+    @NotBlank
     private String frontendUrl;
+
+    @NotBlank
     private String jwtBase64Secret;
+
+    @NotNull
+    @Positive
     private Long apiKeyLength;
+
+    @NotNull
+    @Positive
     private Long shortUrlLength;
+
+    @NotNull
+    @Positive
     private Long jwtTokenValiditySeconds;
+
+    @NotNull
+    @Positive
     private Long apiKeyCallsLimit;
+
+    @NotNull
+    @Positive
     private Long apiKeyExpirationInMonths;
 }
