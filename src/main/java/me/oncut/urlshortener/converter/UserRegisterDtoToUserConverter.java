e4d920befbd54e3cbd734f02d251c29ef0dad210
Karlo Hrvačić <karlo.hrvacic@biss.hr>
2022-05-29 16:43:02 +0200

    Register dto

--- src/main/java/me/oncut/urlshortener/converter/UserRegisterDtoToUserConverter.java
+++ src/main/java/me/oncut/urlshortener/converter/UserRegisterDtoToUserConverter.java
@@ -1,9 +1,13 @@
 package me.oncut.urlshortener.converter;
 
+import lombok.RequiredArgsConstructor;
 import me.oncut.urlshortener.dto.UserRegisterDto;
 import me.oncut.urlshortener.model.User;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class UserRegisterDtoToUserConverter implements Converter<UserRegisterDto, User> {
 
     @Override
