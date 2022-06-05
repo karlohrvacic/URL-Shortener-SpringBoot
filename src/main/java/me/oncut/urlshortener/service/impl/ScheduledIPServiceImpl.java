11d2be93cbd063aa939f51ab420964fb27de9037
Karlo Hrvačić <karlo.hrvacic@biss.hr>
2022-06-05 10:45:53 +0200

    Cron

--- src/main/java/me/oncut/urlshortener/service/impl/ScheduledIPServiceImpl.java
+++ src/main/java/me/oncut/urlshortener/service/impl/ScheduledIPServiceImpl.java
@@ -4,25 +4,29 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
 import me.oncut.urlshortener.service.IPAddressService;
 import me.oncut.urlshortener.service.ScheduledIPService;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
 @CommonsLog
+@EnableScheduling
 @RequiredArgsConstructor
 public class ScheduledIPServiceImpl implements ScheduledIPService {
 
     private final IPAddressService ipAddressService;
 
     @Override
-    @Scheduled(cron = "0 * * * * *")
+    @Scheduled(cron = "* */2 * * * *")
     public void deactivateDeprecatedIps() {
+        log.info("Cron triggered for deactivation of deprecated IPs");
         ipAddressService.deactivateDeprecatedIps();
     }
 
     @Override
-    @Scheduled(cron = "0 */24 * * * *")
+    @Scheduled(cron = "* */2 * * * *")
     public void deleteDeactivatedIps() {
+        log.info("Cron triggered for deletion of deactivated IPs");
         ipAddressService.deleteDeactivatedIps();
     }
 }
