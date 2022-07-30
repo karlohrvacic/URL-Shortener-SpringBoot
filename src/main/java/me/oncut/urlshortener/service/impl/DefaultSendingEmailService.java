b0422c7695d6e951e622f4f0c15ae62b36b1e40c
Karlo Hrvačić <karlohrvacic@gmail.com>
2022-07-30 20:13:13 +0200

    Base64 Fix

--- src/main/java/me/oncut/urlshortener/service/impl/DefaultSendingEmailService.java
+++ src/main/java/me/oncut/urlshortener/service/impl/DefaultSendingEmailService.java
@@ -16,6 +16,7 @@ import me.oncut.urlshortener.model.ResetToken;
 import me.oncut.urlshortener.model.User;
 import me.oncut.urlshortener.service.SendingEmailService;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 import org.thymeleaf.TemplateEngine;
@@ -102,18 +103,19 @@ public class DefaultSendingEmailService implements SendingEmailService {
         ctx.setVariable("contact_email", appProperties.getContactEmail());
         ctx.setVariable("password_reset_link", MessageFormat.format("{0}/#/reset-password", appProperties.getFrontendUrl()));
         ctx.setVariable("token_expiration", appProperties.getResetTokenExpirationInHours().toString());
-        ctx.setVariable("github_image", encodeResourceImageToBase64("github.png"));
-        ctx.setVariable("password_reset_image", encodeResourceImageToBase64("password_reset.png"));
+        ctx.setVariable("github_image", generateBase64Image("github.png"));
+        ctx.setVariable("password_reset_image", generateBase64Image("password_reset.png"));
 
         return ctx;
     }
 
-    private String encodeResourceImageToBase64(final String imageName) {
+    private String generateBase64Image(final String imageName) {
         try {
             final File file = ResourceUtils.getFile("classpath:images/" + imageName);
-            return Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(file));
-        }
-        catch (final IOException exception) {
+
+            return String.format("data:image/%s;base64,%s", FilenameUtils.getExtension(imageName),
+                Base64.getEncoder().encodeToString(FileUtils.readFileToByteArray(file)));
+        } catch (final IOException exception) {
             log.error(exception);
             return "";
         }
