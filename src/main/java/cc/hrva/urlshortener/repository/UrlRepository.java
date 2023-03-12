d31a95975f3f8108b3e7addbe96530e09ca5fad9
Karlo Hrvačić <karlo@hrva.cc>
2023-03-12 23:16:17 +0100

    Removed unnecessary annotation

--- src/main/java/cc/hrva/urlshortener/repository/UrlRepository.java
+++ src/main/java/cc/hrva/urlshortener/repository/UrlRepository.java
@@ -6,10 +6,8 @@ import java.util.Optional;
 import cc.hrva.urlshortener.model.Url;
 import cc.hrva.urlshortener.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.redis.core.RedisHash;
 import org.springframework.stereotype.Repository;
 
-@RedisHash
 @Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
 
